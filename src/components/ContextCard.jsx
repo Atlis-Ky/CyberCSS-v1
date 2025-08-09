e5c5210edc8798177f5c2a7d16431b09fa86680f
Ky <kylesmith97@outlook.com>
2025-08-09 19:47:51 +0100

    content and grammar updates

--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -12,30 +12,30 @@ const ContextCard = () => {
           </span>
         </h1>
         <p>
-          A lightweight, modern looking CSS file that you can use to style your
-          own projects. An ideal way to get your HTML based elements looking
+          CYBER.cSS is a lightweight, modern looking CSS file that you can use to style your
+          own projects. It's an ideal way to get your HTML based elements looking
           fresh super speedily by having access to a wide range of preset CSS
-          classes. Feel free to play around and customise anything in there! you
+          classes. Feel free to play around and customise anything in there! You
           can switch up the colours and styles anywhere you like to personalise
-          things for your own websites!
+          things for your own websites.
         </p>
 
         <h3 className="mt-3">How To Use</h3>
         <p>
-          Easy! Just head to the rep and either download the cyber.css file into
-          your project, or just paste the file into your own code, super
+          That's easy! Just head to the repo and either download the cyber.css file into
+          your project, or paste the file into your own code, super
           straightforward stuff. The stylesheet is designed to target both
-          generic and some unique classes, I'd reccomend having a quick scroll
+          generic and some unique classes, I'd recommend having a quick scroll
           through the sections, and using the shortcut <code>⌘ + F</code> on MacOS,
           or <code>CTRL + F</code> on windows to find any specific classes you're looking to
           target.
         </p>
 
         <div style={{ marginTop: "1.5rem" }}>
-          <button className="btn-secondary">View on GitHub</button>
+          <button className="btn-primary">View on GitHub</button>
         </div>
 
-        <h3 className="mt-3">Full List of Demos Below:</h3>
+        <h3 className="mt-3">List of Demos Below:</h3>
         <hr />
       </div>
     </section>
