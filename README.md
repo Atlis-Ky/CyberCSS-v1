5daca6a6aa94b6d2374ca1c25f33955d7ff9030c
Ky <kylesmith97@outlook.com>
2025-08-09 19:52:53 +0100

    button set up to navigate to this repo, readme updated

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# React + Vite
-
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+   CYBER.CSS is a lightweight, modern looking CSS file that you can use
+          to style your own projects. It's an ideal way to get your HTML based
+          elements looking fresh super speedily by having access to a wide range
+          of preset CSS classes. Feel free to play around and customise anything
+          in there! You can switch up the colours and styles anywhere you like
+          to personalise things for your own websites.
--- src/components/ContextCard.jsx
+++ src/components/ContextCard.jsx
@@ -12,27 +12,38 @@ const ContextCard = () => {
           </span>
         </h1>
         <p>
-          CYBER.CSS is a lightweight, modern looking CSS file that you can use to style your
-          own projects. It's an ideal way to get your HTML based elements looking
-          fresh super speedily by having access to a wide range of preset CSS
-          classes. Feel free to play around and customise anything in there! You
-          can switch up the colours and styles anywhere you like to personalise
-          things for your own websites.
+          CYBER.CSS is a lightweight, modern looking CSS file that you can use
+          to style your own projects. It's an ideal way to get your HTML based
+          elements looking fresh super speedily by having access to a wide range
+          of preset CSS classes. Feel free to play around and customise anything
+          in there! You can switch up the colours and styles anywhere you like
+          to personalise things for your own websites.
         </p>
 
         <h3 className="mt-3">How To Use</h3>
         <p>
-          That's easy! Just head to the repo and either download the cyber.css file into
-          your project, or paste the file into your own code, super
+          That's easy! Just head to the repo and either download the cyber.css
+          file into your project, or paste the file into your own code, super
           straightforward stuff. The stylesheet is designed to target both
           generic and some unique classes, I'd recommend having a quick scroll
-          through the sections, and using the shortcut <code>⌘ + F</code> on MacOS,
-          or <code>CTRL + F</code> on windows to find any specific classes you're looking to
-          target.
+          through the sections, and using the shortcut <code>⌘ + F</code> on
+          MacOS, or <code>CTRL + F</code> on windows to find any specific
+          classes you're looking to target.
         </p>
 
         <div style={{ marginTop: "1.5rem" }}>
-          <button className="btn-primary">View on GitHub</button>
+          <button
+            className="btn-primary"
+            onClick={() =>
+              window.open(
+                "https://github.com/Atlis-Ky/CyberCSS-v1",
+                "_blank",
+                "noopener,noreferrer"
+              )
+            }
+          >
+            View on GitHub
+          </button>
         </div>
 
         <h3 className="mt-3">List of Demos Below:</h3>
